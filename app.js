8671a54446f906e8b582a2f18ee24cb7ff3d0fdf
Ava Tullis <AVA.TULLIS2089@STU.MTEC.EDU>
2023-03-15 19:54:32 -0600

    first commit

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ $('.navItem').css('color', 'white');
 $('#nav').css('background-color', '#145')
 $('li:even').css('background-color','#ccc')
 $('li:odd').css('background-color', '#aaa')
-$(':input').val("Ava")
+$(':input').val("Ava") 
